9e20a46db3d7b487daf08ae440ddb5b128092c2c
hkidd <harrisonakidd@gmail.com>
2021-08-19 15:52:50 -0500

    removed comments

--- server.js
+++ server.js
@@ -26,7 +26,3 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-// Need manifest.json
-// Need service worker
-// Potentially indexedDB as well
